36447a6afca798f3ce6b356c47d0e54f9b438d20
Berzaf Berhane <berzafberhane@Berzafs-MacBook-Pro-2.local>
2020-09-18 14:19:21 -0500

    name format

---
+++ NameFormatter/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace NameFormatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.titleNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.middleNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.middleNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.btnFormatName = new System.Windows.Forms.Button();
+            this.outPut = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(164, 42);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.firstNameLabel.TabIndex = 4;
+            this.firstNameLabel.Text = "First Name";
+            // titleNameTextBox
+            // 
+            this.titleNameTextBox.Location = new System.Drawing.Point(247, 227);
+            this.titleNameTextBox.Name = "titleNameTextBox";
+            this.titleNameTextBox.Size = new System.Drawing.Size(128, 20);
+            this.titleNameTextBox.TabIndex = 5;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(247, 161);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(128, 20);
+            this.lastNameTextBox.TabIndex = 0;
+            // 
+            // middleNameTextBox
+            // 
+            this.middleNameTextBox.Location = new System.Drawing.Point(247, 97);
+            this.middleNameTextBox.Name = "middleNameTextBox";
+            this.middleNameTextBox.Size = new System.Drawing.Size(128, 20);
+            this.middleNameTextBox.TabIndex = 1;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(247, 35);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(128, 20);
+            this.firstNameTextBox.TabIndex = 2;
+            // 
+            // middleNameLabel
+            // 
+            this.middleNameLabel.AutoSize = true;
+            this.middleNameLabel.Location = new System.Drawing.Point(153, 104);
+            this.middleNameLabel.Name = "middleNameLabel";
+            this.middleNameLabel.Size = new System.Drawing.Size(69, 13);
+            this.middleNameLabel.TabIndex = 6;
+            this.middleNameLabel.Text = "Middle Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(164, 168);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(58, 13);
+            this.lastNameLabel.TabIndex = 7;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(195, 234);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(27, 13);
+            this.titleLabel.TabIndex = 8;
+            this.titleLabel.Text = "Title";
+            // 
+            // btnFormatName
+            // 
+            this.btnFormatName.Location = new System.Drawing.Point(167, 355);
+            this.btnFormatName.Name = "btnFormatName";
+            this.btnFormatName.Size = new System.Drawing.Size(104, 23);
+            this.btnFormatName.TabIndex = 10;
+            this.btnFormatName.Text = "Format Name";
+            this.btnFormatName.UseVisualStyleBackColor = true;
+            this.btnFormatName.Click += new System.EventHandler(this.btnFormatName_Click);
+            // 
+            // outPut
+            // 
+            this.outPut.FormattingEnabled = true;
+            this.outPut.Location = new System.Drawing.Point(543, 94);
+            this.outPut.Name = "outPut";
+            this.outPut.Size = new System.Drawing.Size(197, 160);
+            this.outPut.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outPut);
+            this.Controls.Add(this.btnFormatName);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.middleNameLabel);
+            this.Controls.Add(this.titleNameTextBox);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.middleNameTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox titleNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox middleNameTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label middleNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button btnFormatName;
+        private System.Windows.Forms.ListBox outPut;
+    }
+}
+
---
+++ NameFormatter/Form1.cs
@@ -0,0 +1,65 @@
+﻿/*
+Programmer Name: Berzaf Teklu
+Date: 9/12/2020
+
+
+Create an application that lets the user enter the following pieces of data:
+    The user's first name
+    The user's miidle name
+    The user's last name
+    The user's preferred title(Mr., Mrs, Ms., Dr., etc)
+
+Assume the user has entered the following data: 
+    First name: Kelly 
+    Middle name: Jane 
+    Last name: Smith 
+    Title: Ms.
+The application should have buttons that display the user’s name formatted in the following ways: 
+    Ms.Kelly Jane Smith 
+    Kelly Jane Smith 
+    Kelly Smith 
+    Smith, Kelly Jane, Ms. 
+    Smith, Kelly Jane 
+    Smith, Kelly
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NameFormatter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnFormatName_Click(object sender, EventArgs e)
+        {
+            string firstName;
+            string middleName;
+            string lastName;
+            string title;
+            firstName = firstNameTextBox.Text;
+            middleName = middleNameTextBox.Text;
+            lastName = lastNameTextBox.Text;
+            title = titleNameTextBox.Text;
+
+            outPut.Items.Add(title + " " + firstName + " " + middleName + " " + lastName);           //Ms. Kelly Jane Smith 
+            outPut.Items.Add(firstName + " " + middleName + " " + lastName);                         //Kelly Jane Smith 
+            outPut.Items.Add(firstName + " " + lastName);                                            //Kelly Smith
+            outPut.Items.Add(lastName + ", " + firstName + " " + middleName + ", " + title);         //Smith, Kelly Jane, Ms.
+            outPut.Items.Add(lastName + ", " + firstName + " " + middleName);                        //Smith, Kelly Jane
+            outPut.Items.Add(lastName + ", " + firstName);                                           //Smith, Kelly
+
+        }
+
+
+    }
+}
